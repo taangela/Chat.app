e6aace4f99ffbc43aa7cfc2594c5914b5d1637a9
Marta Koperek <makoperek@gmail.com>
2020-02-20 14:50:31 +0100

    Add join event and users array

--- client/app.js
+++ client/app.js
@@ -1,59 +1,66 @@
-'use strict';
-{
-  const loginForm = document.getElementById('welcome-form');
-  const messageSection = document.getElementById('messages-section');
-  const messagesList = document.getElementById('messages-list');
-  const addMesssageForm = document.getElementById('add-messages-form');
-  const userNameInput = document.getElementById('username');
-  const messageContentInput =document.getElementById('message-content');
-
-  let userName ='';
+const socket = io();//zainicjuj nowego klienta socketowego i zachowaj referencje do niego pod stałą socket
+//socket.on('message', (event) => addMessage(event.author, event.content))
+socket.on('message', ({ author, content }) => addMessage(author, content))
 
 
-  function login(event) {
-    event.preventDefault();
-    if (!userNameInput.value) {
-      window.alert('You need to type your name!');
-      return;
-    }else {
-      userName = userNameInput.value;
-      loginForm.classList.remove('show');
-      messageSection.classList.add('show');
-    }
+const loginForm = document.getElementById('welcome-form');
+const messageSection = document.getElementById('messages-section');
+const messagesList = document.getElementById('messages-list');
+const addMesssageForm = document.getElementById('add-messages-form');
+const userNameInput = document.getElementById('username');
+const messageContentInput =document.getElementById('message-content');
+
+let userName ='';
+
+
+function login(event) {
+  event.preventDefault();
+  if (!userNameInput.value) {
+    alert('You need to type your name!');
+    return;
+  }else {
+    userName = userNameInput.value;
+    socket.emit('join', userName);
+    loginForm.classList.remove('show');
+    messageSection.classList.add('show');
   }
-
-  function addMessage(author, content) {
-    const message = document.createElement('li');
-    message.classList.add('message');
-    message.classList.add('message--received');
-    if (author === userName) message.classList.add('message--self');
-    message.innerHTML = `
-    <h3 class="message__author">${userName === author ? 'You' : author}</h3>
-    <div class="message__content">
-      ${content}
-    </div>
-  `;
-    messagesList.appendChild(message);
-
-  }
-  function sendMessage(event) {
-    event.preventDefault();
-    if (!messageContentInput.value) {
-      alert('Yoy need write your message!');
-      return;
-    }else {
-      addMessage(userName, messageContentInput.value);
-      messageContentInput.value='';  
-    }
-  }
-
-
-
-  loginForm.addEventListener('submit', event => {
-    login(event);
-  });
-
-  addMesssageForm.addEventListener('submit', event => {
-    sendMessage(event);
-  });
 }
+
+function addMessage(author, content) {
+  const message = document.createElement('li');
+  message.classList.add('message');
+  message.classList.add('message--received');
+  if (author === userName) message.classList.add('message--self');
+  message.innerHTML = `
+  <h3 class="message__author">${userName === author ? 'You' : author}</h3>
+  <div class="message__content">
+    ${content}
+  </div>
+`;
+  messagesList.appendChild(message);
+
+}
+function sendMessage(event) {
+  event.preventDefault();
+
+  let messageContent = messageContentInput.value;
+
+  if (!messageContent.length) {
+    alert('Yoy need to write your message!');
+    return;
+  }else {
+    addMessage(userName, messageContent);
+    socket.emit('message', { author: userName, content: messageContent })
+    messageContentInput.value='';  
+  }
+}
+
+
+
+loginForm.addEventListener('submit', event => {
+  login(event);
+});
+
+addMesssageForm.addEventListener('submit', event => {
+  sendMessage(event);
+});
--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const path = require('path');
+const socket = require('socket.io');
 
 const app = express();
 
+
 const messages = [];
+let users =[];
 
 app.use(express.static(path.join(__dirname + '/client')));
 
@@ -11,7 +14,28 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/client/index.html'));
 });
 
-
-app.listen(8000, () => {
+const server = app.listen(8000, () => {
   console.log('Server is running on port: 8000');
+});
+
+const io = socket(server);
+
+io.on('connection', (socket) => {
+  console.log('New client! Its id – ' + socket.id);
+  socket.on('join',(user) => {
+    users.push({name: user, id: socket.id});
+    console.log(users);
+  });
+  socket.on('message', (message) => {
+    console.log('Oh, I\'ve got something from ' + socket.id);
+    messages.push(message);
+    socket.broadcast.emit('message', message);
+  });
+  socket.on('disconnect', () => {
+    users = users.filter(el => el.id !== socket.id); 
+    console.log('Oh, socket ' + socket.id + ' has left') 
+    console.log(users);
+
+  });
+  console.log('I\'ve added a listener on message and disconnect events \n');
 });
